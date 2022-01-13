41f9ac57eb4b8dcea1639ae81c7d160cc2270d68
megancozort <mc0182@mix.wvu.edu>
2022-01-13 14:19:23 -0500

    test

--- Task_01/task01.r
+++ Task_01/task01.r
@@ -1,6 +1,26 @@
 # password for git via terminal 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
 #install.packages ("swirl")
 library (swirl)
-
-# this is a test comment
-# test 2
+swirl()
+Megan Cozort
+1
+1
+1
+5+7
+x<-5+7
+x
+y<-x-3
+y
+z<-c(1.1,9,3.14)
+?c
+z
+c(z, 555 , z)
+z * 2 + 1000
+my_sqrt<-sqrt(z-1)
+2
+my_div <- z/ my_sqrt
+3
+my_div
+c(1,2,3,4)+ c(0,10)
+my_div
+1
