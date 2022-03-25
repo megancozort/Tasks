b35a7d64ef1cfe9f8fec1c3f432b5afd09c11f5b
megancozort <mc0182@mix.wvu.edu>
2022-03-24 23:35:25 -0400

    <MeganCozort> Task 09 upload

--- Task_09/task09.r
+++ Task_09/task09.r
@@ -15,8 +15,11 @@ svl
 Ancestors<- fastAnc(tree, svl, vars=TRUE, CI=TRUE)
 head(Ancestors)
 ?fastAnc
-#Q3- CI95 is the variance on a 95% confidence intervals, 
-#Q4- Two assumptions made 
+#Q3- Estimated values stored in the nodes
+#CI95 is the variance on a 95% confidence intervals, 
+#Q4- Two assumptions made for fastAnc is that the estimates from the data falls 
+#within the 95% confidence interval and that state compound for root node equals 
+#the MLE of the root node 
 par(mar=c(0.1,0.1,0.1,0.1))
 plot(tree, type="fan", lwd=2, show.tip.label=F)
 tiplabels(pch=16, cex=0.25*svl[tree$tip.label])
@@ -44,5 +47,40 @@ Ancestors_withFossils<- fastAnc(tree, svl, anc.states=fossilNodes,
 
 
 #Q7- 
+head(Ancestors)
+head(Ancestors_withFossils)
+Ancestors_withFossils
+Ancestors_withoutFossils <- fastAnc(tree, svl, CI=TRUE, var=TRUE)
+Ancestors_withoutFossils
+plot(Ancestors_withFossils$ace, Ancestors_withoutFossils$ace, xlab='with fossils'
+     , ylab= 'without fossils')
+#The plot increased the overall ancestral size 
+     
+
+#Q8-
+install.packages("geiger")
+library(geiger)
+lambdaFit<- fitContinuous(tree, data, model="lambda")
+#AIC= -4.51
+brownianFit<- fitContinuous(tree, data, model="BM")
+#AIC= -6.51
+ouFit<- fitContinuous(tree, data, model="OU")
+#AIC=-4.51
+ebFit<- fitContinuous(tree, data, model="EB")
+#AIC= -7.24
+ratetrendFit<- fitContinuous(tree, data, model="rate_trend")
+#AIC= -6.98 
+kappaFit<- fitContinuous(tree, data, model="kappa")
+#AIC= -4.51
+deltaFit<- fitContinuous(tree, data, model="delta")
+#AIC= -6.11
+meantrendFit <- fitContinuous( tree, data, model="mean_trend")
+#AIC= -4.51
+whiteFit<- fitContinuous(tree, data, model="white")
+#AIC= 91.39 
+# Model ebFit is the best fit model for the data becuase it has the lowest AIC
+#FastAnc performs fast estimation of ML ancestral states while fitContinuous 
+#compares continuous comparative data. Both assume continous traits and 
+#and have the same confidence intervals. 
 
 
